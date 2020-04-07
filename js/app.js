9644857a5b744d52546757c2195aa36d7143062c
korene korologos <korenekorologos@gmail.com>
2020-04-07 11:44:43 -0600

    flame from back of ship has been added. along with coding the rocket to come back on the page once it hits the sides of the page.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const FPS = 30; //the frames per second 
+const FRICTION = 0.7; //friction coefficient of space (0 = no friction, 1 = lots of friction)
 const SHIP_SIZE = 30;  //ship height in pixels 
+const SHIP_PUSH = 5; //acceleration of the ship in pixels per second. each second increases by 5  
 const TURN_SPEED = 360; //turn speed in the degrees per second 
 
 
@@ -19,11 +21,15 @@ var ship = {
     pushing: false,  //not stoping 
     push: { //sets the magnitude for the push 
         x: 0,
-        y: 0 
+        y: 0
     }
 }
 
-//set up event handlers 
+//astroids 
+
+
+
+//event handlers 
 //type = "keydown". alerted when one of the keys are pressed 
 //function called = keyDown 
 document.addEventListener("keydown", keyDown);
@@ -31,7 +37,6 @@ document.addEventListener("keyup", keyUp); //when the key is released
 
 
 //game loop, for animation 
-//FPS = frames per second 
 setInterval(update, 1000 / FPS);
 
 
@@ -42,8 +47,8 @@ function keyDown(/** @type {KeyboardEvent} */ ev) {
             ship.rot = TURN_SPEED / 180 * Math.PI / FPS;
             break;
         case 38: //up arrow (moves the ship forward)
-        ship.pushing = true;    
-        break;
+            ship.pushing = true;
+            break;
         case 39: //right arrow (rotates the ship right)
             ship.rot = -TURN_SPEED / 180 * Math.PI / FPS;
             break;
@@ -58,8 +63,8 @@ function keyUp(/** @type {KeyboardEvent} */ ev) {
             ship.rot = 0;
             break;
         case 38: //up arrow (stop moving the ship forward)
-        ship.pushing = false;     
-        break;
+            ship.pushing = false;
+            break;
         case 39: //right arrow (stop rotating the ship right)
             ship.rot = 0;
             break;
@@ -72,12 +77,46 @@ function update() {
     ctx.fillRect(0, 0, canv.width, canv.height);
 
     //push the ship
-    if(ship.pushing) {
-        
+    //FPS = frames per second 
+    if (ship.pushing) {
+        ship.push.x += SHIP_PUSH * Math.cos(ship.a) / FPS;
+        ship.push.y -= SHIP_PUSH * Math.sin(ship.a) / FPS;
+
+
+        //flame from the rocket 
+        ctx.fillStyle = "red"; 
+        ctx.strokeStyle = "yellow";
+            ctx.lineWidth = SHIP_SIZE / 10; //makes for a thicker line
+        ctx.beginPath();
+
+        ctx.moveTo( //rear left 
+            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + 0.5 * Math.sin(ship.a)),  
+            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - 0.5 * Math.cos(ship.a))
+        );
+
+        ctx.lineTo(  //rear center, behind the ship 
+            ship.x - ship.r * 6 / 3 * Math.cos(ship.a),  
+            ship.y + ship.r * 6 / 3 * Math.sin(ship.a)
+        );
+
+        ctx.lineTo(  //rear right of the ship 
+            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - 0.5 * Math.sin(ship.a)),  
+            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + 0.5 * Math.cos(ship.a))
+        );
+
+        ctx.closePath();  
+        ctx.fill(); 
+        ctx.stroke();
+
+
+    } else {
+        ship.push.x -= FRICTION * ship.push.x / FPS;
+        ship.push.y -= FRICTION * ship.push.y / FPS;
     }
 
+
     //draw triangular ship 
-    ctx.strokeStyle = "white",
+    ctx.strokeStyle = "white"; 
         ctx.lineWidth = SHIP_SIZE / 20;
     ctx.beginPath();
 
@@ -101,11 +140,29 @@ function update() {
 
     //rotate ship 
     ship.a += ship.rot;
+
     //move the ship
+    ship.x += ship.push.x;
+    ship.y += ship.push.y;
+
+    //so that the rocket doesn't get lost off of the screen. this will make it so
+    //that it comes back to the screen 
+    if (ship.x < 0 - ship.r) {
+        ship.x = canv.width + ship.r;
+    } else if (ship.x > canv.width + ship.r) {
+        ship.x = 0 - ship.r;
+    }
+    if (ship.y < 0 - ship.r) {
+        ship.y = canv.height + ship.r;
+    } else if (ship.y > canv.height + ship.r) {
+        ship.y = 0 - ship.r;
+    }
 
     //centre dot 
 
+
+
     ctx.fillStyle = "red";
-    ctx.fillRect(ship.x - 1, ship.y - 1, 2, 2);
+    //ctx.fillRect(ship.x - 1, ship.y - 1, 2, 2);
 
 }
