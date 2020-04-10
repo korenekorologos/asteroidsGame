0f749ff45019029b0365c065f1742033d9597307
korene korologos <korenekorologos@gmail.com>
2020-04-10 12:57:42 -0600

    explosion is happening when ship hits astroids and then it refreshes the ship to original position after 3 seconds of explosion

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 const FPS = 30; //the frames per second 
 const FRICTION = 0.7; //friction coefficient of space (0 = no friction, 1 = lots of friction)
-const ROIDS_JAG = 0.4; //jaggedness of the asteroids. 0 = none. 1 = lots 
-const ROIDS_NUM = 7; //starting number of astroids 
-const ROIDS_SIZE = 100; //starting size of the astroids in pixels 
+const ROID_JAG = 0.4; //jaggedness of the asteroids. 0 = none. 1 = lots 
+const ROID_NUM = 7; //starting number of astroids 
+const ROID_SIZE = 100; //starting size of the astroids in pixels 
 const ROID_SPD = 50; //max starting speed of astroids in pizels per second
-const ROIDS_VERT = 10; //average number of vertices on each asteroid
+const ROID_VERT = 10; //average number of vertices on each asteroid
+const SHIP_BLINK_DUR = 0.1; //duration of the ships blink during invisibility in seconds 
 const SHIP_EXPLODE_DUR = 0.3; //duration of the ships explosion 
+const SHIP_INV_DUR = 3; //duration of the ships invisibility in seconds  
 const SHIP_SIZE = 30;  //ship height in pixels 
 const SHIP_PUSH = 5; //acceleration of the ship in pixels per second. each second increases by 5  
-const TURN_SPEED = 360; //turn speed in the degrees per second 
+const TURN_SPD = 360; //turn speed in the degrees per second 
 const SHOW_BOUNDING = false;  //show or hide collision bounding 
 const SHOW_CENTRE_DOT = false; //show or hide ships center dot 
 
@@ -16,26 +18,25 @@ const SHOW_CENTRE_DOT = false; //show or hide ships center dot
 /**@type {HTML CanvasElement} */
 //canvas info 
 var canv = document.getElementById("gameCanvas");
-
 //context from canvas 
 var ctx = canv.getContext("2d");
+//spaceshit object 
+var ship = newShip();
 
-var ship = newShip(); 
 
-
-var ship = {
-    x: canv.width / 2,
-    y: canv.height / 2,
-    r: SHIP_SIZE / 2,   //radius 
-    a: 90 / 180 * Math.PI,  //a = angle. convert 90 into radians 
-    explodeTime: 0, //zero frames left of exploding 
-    rot: 0, //rotation 
-    pushing: false,  //not stoping 
-    push: { //sets the magnitude for the push 
-        x: 0,
-        y: 0
-    }
-}
+// var ship = {
+//     x: canv.width / 2,
+//     y: canv.height / 2,
+//     r: SHIP_SIZE / 2,   //radius 
+//     a: 90 / 180 * Math.PI,  //a = angle. convert 90 into radians 
+//     explodeTime: 0, //zero frames left of exploding 
+//     rot: 0, //rotation 
+//     pushing: false,  //not stoping 
+//     push: { //sets the magnitude for the push 
+//         x: 0,
+//         y: 0
+//     }
+// }
 
 //astroids 
 var roids = [];
@@ -57,12 +58,12 @@ function createAsteroidBelt() {
     roids = [];
     var x, y;
     //loop
-    for (var i = 0; i < ROIDS_NUM; i++) {
+    for (var i = 0; i < ROID_NUM; i++) {
         //makes it so that asteriod is not positioned on the rocket when starting the game
         do {
             x = Math.floor(Math.random() * canv.width);
             y = Math.floor(Math.random() * canv.height);
-        } while (distBetweenPoints(ship.x, ship.y, x, y) < ROIDS_SIZE * 2 + ship.r);
+        } while (distBetweenPoints(ship.x, ship.y, x, y) < ROID_SIZE * 2 + ship.r);
         roids.push(newAsteroid(x, y));
     }
 }
@@ -116,15 +117,15 @@ function newAsteroid(x, y) {
         //positive and negative direction they're going 
         xv: Math.random() * ROID_SPD / FPS * (Math.random() < 0.5 ? 1 : -1),
         yv: Math.random() * ROID_SPD / FPS * (Math.random() < 0.5 ? 1 : -1),
-        r: ROIDS_SIZE / 2,
+        r: ROID_SIZE / 2,
         a: Math.random() * Math.PI * 2, //in radians 
-        vert: Math.floor(Math.random() * (ROIDS_VERT + 1) + ROIDS_VERT / 2),
+        vert: Math.floor(Math.random() * (ROID_VERT + 1) + ROID_VERT / 2),
         offs: []
     };
 
     //create the vertex which offsets array 
     for (var i = 0; i < roid.vert; i++) {
-        roid.offs.push(Math.random() * ROIDS_JAG * 2 + 1 - ROIDS_JAG);
+        roid.offs.push(Math.random() * ROID_JAG * 2 + 1 - ROID_JAG);
     }
 
     return roid;
@@ -133,31 +134,75 @@ function newAsteroid(x, y) {
 
 function newShip() {
     return {
-            x: canv.width / 2,
-            y: canv.height / 2,
-            r: SHIP_SIZE / 2,   //radius 
-            a: 90 / 180 * Math.PI,  //a = angle. convert 90 into radians 
-            explodeTime: 0, //zero frames left of exploding 
-            rot: 0, //rotation 
-            pushing: false,  //not stoping 
-            push: { //sets the magnitude for the push 
-                x: 0,
-                y: 0
-            }
+        x: canv.width / 2,
+        y: canv.height / 2,
+        r: SHIP_SIZE / 2,   //radius 
+        a: 90 / 180 * Math.PI,  //a = angle. convert 90 into radians 
+        blinkNum: Math.ceil(SHIP_INV_DUR / SHIP_BLINK_DUR),  //15 blinks off, 15 blinks on 
+        blinkTime: Math.ceil(SHIP_BLINK_DUR * FPS),
+        explodeTime: 0, //zero frames left of exploding 
+        rot: 0, //rotation 
+        pushing: false,  //not stoping 
+        push: { //sets the magnitude for the push 
+            x: 0,
+            y: 0
         }
-        
     }
-
+}
 
 
 
 function update() {
+    var blinkOn = ship.blinkNum % 2 == 0;
     var exploding = ship.explodeTime > 0;
 
 
     //draw space 
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canv.width, canv.height);
+
+
+ // draw the asteroids
+ var a, r, x, y, offs, vert;
+ for (var i = 0; i < roids.length; i++) {
+     ctx.strokeStyle = "slategrey";
+     ctx.lineWidth = SHIP_SIZE / 20;
+
+     // get the asteroid properties
+     a = roids[i].a;
+     r = roids[i].r;
+     x = roids[i].x;
+     y = roids[i].y;
+     offs = roids[i].offs;
+     vert = roids[i].vert;
+     
+     // draw the path
+     ctx.beginPath();
+     ctx.moveTo(
+         x + r * offs[0] * Math.cos(a),
+         y + r * offs[0] * Math.sin(a)
+     );
+
+     // draw the polygon
+     for (var j = 1; j < vert; j++) {
+         ctx.lineTo(
+             x + r * offs[j] * Math.cos(a + j * Math.PI * 2 / vert),
+             y + r * offs[j] * Math.sin(a + j * Math.PI * 2 / vert)
+         );
+     }
+     ctx.closePath();
+     ctx.stroke();
+
+     // show asteroid's collision circle
+     if (SHOW_BOUNDING) {
+         ctx.strokeStyle = "lime";
+         ctx.beginPath();
+         ctx.arc(x, y, r, 0, Math.PI * 2, false);
+         ctx.stroke();
+     }
+ }
+
+
 
     //push the ship
     //FPS = frames per second 
@@ -167,7 +212,7 @@ function update() {
 
 
         //flame from the rocket 
-        if (!exploding) {
+        if (!exploding && blinkOn) {
             ctx.fillStyle = "red";
             ctx.strokeStyle = "yellow";
             ctx.lineWidth = SHIP_SIZE / 10; //makes for a thicker line
@@ -187,7 +232,7 @@ function update() {
                 ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - 0.5 * Math.sin(ship.a)),
                 ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + 0.5 * Math.cos(ship.a))
             );
-
+        
             ctx.closePath();
             ctx.fill();
             ctx.stroke();
@@ -210,28 +255,43 @@ function update() {
 
     //draw triangular ship 
     if (!exploding) {
+        if (blinkOn) {  //if blink is on, then we'll draw the ship 
 
-        ctx.strokeStyle = "white";
-        ctx.lineWidth = SHIP_SIZE / 20;
-        ctx.beginPath();
+            ctx.strokeStyle = "white";
+            ctx.lineWidth = SHIP_SIZE / 20;
+            ctx.beginPath();
 
-        ctx.moveTo( //nose of the ship 
-            ship.x + 4 / 3 * ship.r * Math.cos(ship.a),
-            ship.y - 4 / 3 * ship.r * Math.sin(ship.a) //negative represents upwards on the screen 
-        );
+            ctx.moveTo( //nose of the ship 
+                ship.x + 4 / 3 * ship.r * Math.cos(ship.a),
+                ship.y - 4 / 3 * ship.r * Math.sin(ship.a) //negative represents upwards on the screen 
+            );
 
-        ctx.lineTo(  //rear left of the ship 
-            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + Math.sin(ship.a)),  //got these equations by using trigonometry 
-            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - Math.cos(ship.a))
-        );
+            ctx.lineTo(  //rear left of the ship 
+                ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + Math.sin(ship.a)),  //got these equations by using trigonometry 
+                ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - Math.cos(ship.a))
+            );
 
-        ctx.lineTo(  //rear right of the ship 
-            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - Math.sin(ship.a)),  //got these equations by using trigonometry 
-            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + Math.cos(ship.a))
-        );
+            ctx.lineTo(  //rear right of the ship 
+                ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - Math.sin(ship.a)),  //got these equations by using trigonometry 
+                ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + Math.cos(ship.a))
+            );
 
-        ctx.closePath();  //closes the lines from the right & left rears 
-        ctx.stroke();
+            ctx.closePath();  //closes the lines from the right & left rears 
+            ctx.stroke();
+        }
+
+        //handle blinking 
+        if (ship.blinkNum > 0) {
+            //reduce the blink time 
+            ship.blinkTime--;
+
+            //reduce the blink num 
+            if (ship.blinkTime == 0) {
+                ship.blinkTime = Math.ceil(SHIP_BLINK_DUR * FPS);
+                ship.blinkNum--;
+            }
+        }
+
     } else {
         //draw the explosion 
         ctx.fillStyle = "darkred";
@@ -321,61 +381,64 @@ function update() {
 
 
     //create asteroid collisions 
-    if (!exploding) { 
-    for (var i = 0; i < roids.length; i++) {
-        if (distBetweenPoints(ship.x, ship.y, roids[i].x, roids[i].y) < ship.r + roids[i].r) {
-            explodeShip();
+    if (!exploding) {
+        //notcheck if not blinking 
+        if (ship.blinkNum == 0) {
+            for (var i = 0; i < roids.length; i++) {
+                if (distBetweenPoints(ship.x, ship.y, roids[i].x, roids[i].y) < ship.r + roids[i].r) {
+                    explodeShip();
+                }
+            }
+        }
+
+        //rotate ship 
+        ship.a += ship.rot;
+
+        //move the ship
+        ship.x += ship.push.x;
+        ship.y += ship.push.y;
+    } else {
+        ship.explodeTime--; //reduces the time left on the explosion 
+
+        if (ship.explodeTime == 0) {
+            ship = newShip();
         }
     }
 
-    //rotate ship 
-    ship.a += ship.rot;
-
-    //move the ship
-    ship.x += ship.push.x;
-    ship.y += ship.push.y;
-} else {
-    ship.explodeTime --; //reduces the time left on the explosion 
-
-    if (ship.explodeTime == 0) {
-        ship = newShip(); 
+    //so that the rocket doesn't get lost off of the screen. this will make it so
+    //that it comes back to the screen 
+    if (ship.x < 0 - ship.r) {
+        ship.x = canv.width + ship.r;
+    } else if (ship.x > canv.width + ship.r) {
+        ship.x = 0 - ship.r;
     }
-}
-
-//so that the rocket doesn't get lost off of the screen. this will make it so
-//that it comes back to the screen 
-if (ship.x < 0 - ship.r) {
-    ship.x = canv.width + ship.r;
-} else if (ship.x > canv.width + ship.r) {
-    ship.x = 0 - ship.r;
-}
-if (ship.y < 0 - ship.r) {
-    ship.y = canv.height + ship.r;
-} else if (ship.y > canv.height + ship.r) {
-    ship.y = 0 - ship.r;
-}
-
-//moves the asteroids, in different speeds & directions
-for (var i = 0; i < roids.length; i++) {
-    roids[i].x += roids[i].xv;
-    roids[i].y += roids[i].yv;
-
-    //makes it so that astroids don't disapear off the screen 
-    //for the x direction 
-    if (roids[i].x < 0 - roids[i].r) {
-        roids[i].x = canv.width + roids[i].r;
-    } else if (roids[i].x > canv.width + roids[i].r) {
-        roids[i].x = 0 - roids[i].r
+    if (ship.y < 0 - ship.r) {
+        ship.y = canv.height + ship.r;
+    } else if (ship.y > canv.height + ship.r) {
+        ship.y = 0 - ship.r;
     }
 
-    //for the y direction 
-    if (roids[i].y < 0 - roids[i].r) {
-        roids[i].y = canv.height + roids[i].r;
-    } else if (roids[i].y > canv.height + roids[i].r) {
-        roids[i].y = 0 - roids[i].r
-    }
+    //moves the asteroids, in different speeds & directions
+    for (var i = 0; i < roids.length; i++) {
+        roids[i].x += roids[i].xv;
+        roids[i].y += roids[i].yv;
 
-}
+        //makes it so that astroids don't disapear off the screen 
+        //for the x direction 
+        if (roids[i].x < 0 - roids[i].r) {
+            roids[i].x = canv.width + roids[i].r;
+        } else if (roids[i].x > canv.width + roids[i].r) {
+            roids[i].x = 0 - roids[i].r
+        }
+
+        //for the y direction 
+        if (roids[i].y < 0 - roids[i].r) {
+            roids[i].y = canv.height + roids[i].r;
+        } else if (roids[i].y > canv.height + roids[i].r) {
+            roids[i].y = 0 - roids[i].r
+        }
+
+    }
 
     //ctx.fillStyle = "red";
     //ctx.fillRect(ship.x - 1, ship.y - 1, 2, 2);
